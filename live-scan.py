75150f058b49023f44ad065679b0bc7c5d4cc55b
Chase Kanipe <chasekanipe@wireless-10-105-134-74.umd.edu>
2020-05-08 18:06:37 -0400

    done

---
+++ live-scan.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#------------------------------------------------------------------------
+# pylive: ex-live-scan.py
+#
+# Demonstrates scanning the live set and outputs its structure.
+#------------------------------------------------------------------------
+from live import *
+
+import logging
+
+logging.basicConfig(format="%(asctime)-15s %(message)s")
+logging.getLogger("live").setLevel(logging.INFO)
+
+#------------------------------------------------------------------------
+# Scanning a set with clip names and devices gives a more comprehensive
+# overview of the set, but takes significantly longer.
+#------------------------------------------------------------------------
+set = Set()
+set.scan(scan_clip_names=True, scan_devices=True)
+set.dump()
